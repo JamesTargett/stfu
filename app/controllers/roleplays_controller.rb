20a451c9b22d1854dde996ed9f38dad444471910
James Targett <someguynamedjamestargett@gmail.com>
2013-11-24 03:27:39 +0000

    add roleplay implemented

--- app/controllers/roleplays_controller.rb
+++ app/controllers/roleplays_controller.rb
@@ -1,15 +1,35 @@
 class RoleplaysController < ApplicationController
 
-  before_filter :load_match 
+  before_action :load_match 
 
   def index
     @roleplays = @match.roleplays
+    @roleplay = Roleplay.new # not be nil
+  end
+
+  def new
+    @roleplay = Roleplay.new
+  end
+
+  def create
+    @roleplay = @match.roleplays.new(roleplay_params)
+    # FIXME: this is temporary until we have login
+    @roleplay.character = @match.character1 
+    if @roleplay.save
+      redirect_to matches_path, notice: "roleplay was submitted successfully!"
+    else
+      render :new
+    end
   end
 
 protected
 
   def load_match
     @match = Match.find(params[:match_id])
+  end
+
+  def roleplay_params
+    params.require(:roleplay).permit(:message)
   end
 
 end
