21dbe56d404e78502dfccede2fcae44fc466413e
James Targett <someguynamedjamestargett@gmail.com>
2013-12-08 06:58:26 +0000

    moved credit card details to nested factory :subscribed_user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,12 @@ FactoryGirl.define do
     sequence(:email) {|i| "fightclub-#{i}@makeandsellsoap.com" }
     password "hitmeashardasyoucan"
     password_confirmation "hitmeashardasyoucan"
-    name_on_card "Tyler Durden"
-    expiration_month 8
-    expiration_year 17
-    paid true
-    card_token "cus_34LHBWaFYy7z7B"
     factory :subscribed_user do
-        card_info 5555555555555555
-    # check to see if card_token and card_info are redundant
+        name_on_card "Tyler Durden"
+        expiration_month 8
+        expiration_year 17
+        paid true
+        card_token "cus_34LHBWaFYy7z7B"
     end
   end
 end
